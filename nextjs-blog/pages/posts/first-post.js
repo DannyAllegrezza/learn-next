88c47eed8875fe87088320746fef1ce86ab4469e
Danny Allegrezza <danny@dannyallegrezza.com>
2020-12-13 16:55:38 -0500

    Demo: Static assets served from the public directory

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -4,11 +4,13 @@ export default function FirstPost() {
     return (
         <>
             <h1>First Post</h1>
+
             <h2>
                 <Link href="/">
                     <a>Back to home</a>
                 </Link>
             </h2>
+            <img src="/elise.JPG" />
         </>
     )
 }
