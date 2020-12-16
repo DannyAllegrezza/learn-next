538fcc7ea0155dc1f812d2d1c9b90520669abffe
Danny Allegrezza <danny@dannyallegrezza.com>
2020-12-15 21:28:04 -0500

    Demo: Using the <Head> component

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,8 +1,12 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function FirstPost() {
     return (
         <>
+            <Head>
+                <title>My first post</title>
+            </Head>
             <h1>First Post</h1>
 
             <h2>
@@ -10,7 +14,7 @@ export default function FirstPost() {
                     <a>Back to home</a>
                 </Link>
             </h2>
-            <img src="/elise.JPG" />
+            <img src="/elise.JPG" height={500} />
         </>
     )
 }
