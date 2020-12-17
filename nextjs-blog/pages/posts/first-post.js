9c794c48812018b83af097d76e5860b4075be048
DannyAllegrezza <danny@dannyallegrezza.com>
2020-12-16 23:49:23 -0500

    Demo: CSS modules in Next

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,20 +1,19 @@
-import Link from 'next/link';
-import Head from 'next/head';
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
             <Head>
-                <title>My first post</title>
+                <title>First Post</title>
             </Head>
             <h1>First Post</h1>
-
             <h2>
                 <Link href="/">
                     <a>Back to home</a>
                 </Link>
             </h2>
-            <img src="/elise.JPG" height={500} />
-        </>
+        </Layout>
     )
 }
