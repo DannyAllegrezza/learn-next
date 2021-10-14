b10dc533b082cd085746517d664ddeb1d314b68a
DannyAllegrezza <danny@dannyallegrezza.com>
2021-10-13 21:08:52 -0400

    Demo: Dynamic Routes
    https://nextjs.org/learn/basics/dynamic-routes/implement-getstaticprops

--- nextjs-blog/lib/posts.js
+++ nextjs-blog/lib/posts.js
@@ -1,15 +1,9 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 
 const postsDirectory = path.join(process.cwd(), 'posts')
 
-/**
- * Fetches all Markdown files located in the "posts" directory and transforms them into 
- * an object that we can return to our components.
- * 
- * @returns 
- */
 export function getSortedPostsData() {
     // Get file names under /posts
     const fileNames = fs.readdirSync(postsDirectory)
@@ -31,13 +25,36 @@ export function getSortedPostsData() {
         }
     })
     // Sort posts by date
-    return allPostsData.sort(({ date: a }, { date: b }) => {
-        if (a < b) {
+    return allPostsData.sort((a, b) => {
+        if (a.date < b.date) {
             return 1
-        } else if (a > b) {
-            return -1
         } else {
-            return 0
+            return -1
         }
     })
+}
+
+export function getAllPostIds() {
+    const fileNames = fs.readdirSync(postsDirectory)
+    return fileNames.map(fileName => {
+        return {
+            params: {
+                id: fileName.replace(/\.md$/, '')
+            }
+        }
+    })
+}
+
+export function getPostData(id) {
+    const fullPath = path.join(postsDirectory, `${id}.md`)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents)
+
+    // Combine the data with the id
+    return {
+        id,
+        ...matterResult.data
+    }
 }
--- nextjs-blog/pages/posts/secondPost.js
+++
@@ -1,8 +0,0 @@
-export default function SecondPage() {
-    return (
-        <div>
-            <h1>Second page</h1>
-            <p>This file is named <code>secondPost.js</code> and it's route is <code>/posts/secondPost</code></p>
-        </div>
-    )
-}
